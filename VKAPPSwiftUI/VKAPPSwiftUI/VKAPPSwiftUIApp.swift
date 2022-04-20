cb84e790b2a5c93cbe6d89a7b58acfec76106fc7
xokhotax <okhotai@gmail.com>
2022-04-20 19:07:07 +0300

    StartOfTheProject

---
+++ VKAPPSwiftUI/VKAPPSwiftUI/VKAPPSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKAPPSwiftUIApp.swift
+//  VKAPPSwiftUI
+//
+//  Created by Ivan Okhota on 20.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKAPPSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
