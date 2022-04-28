3a813bd763e4ffa24132934e4162d2312e453322
xokhotax <okhotai@gmail.com>
2022-04-28 15:52:16 +0300

    add pods

--- VKAPPSwiftUI/VKAPPSwiftUI/FriendGalleryView.swift
+++ VKAPPSwiftUI/VKAPPSwiftUI/FriendGalleryView.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct FriendGalleryView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
 }
 
 struct FriendGalleryView_Previews: PreviewProvider {
-    static var previews: some View {
-        FriendGalleryView()
-    }
+  static var previews: some View {
+    FriendGalleryView()
+  }
 }
